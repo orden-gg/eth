131141141df4168d823288d414b8bdd8c30125fe
dmutrokot <dmutrokot94@gmail.com>
2022-02-11 12:19:30 +0200

    feat: whitelist from file (#1) @cha0sg0d

--- tasks/whitelist.ts
+++ tasks/whitelist.ts
@@ -121,6 +121,114 @@ async function whitelistExistsKey(args: { key: string }, hre: HardhatRuntimeEnvi
 }
 
 task(
+  'whitelist-nokey:register',
+  'path to csv file of addresses'
+) 
+  .addParam(
+    'path',
+    'file path to csv of addresses',
+    undefined,
+    types.string
+)
+.setAction(noKeyWhitelistRegister);
+
+async function noKeyWhitelistRegister(args: { path: string }, hre: HardhatRuntimeEnvironment) {
+  await hre.run('utils:assertChainId');
+
+  const whitelist: Whitelist = await hre.run('utils:getWhitelist');
+
+  const prevBalance = await hre.ethers.provider.getBalance(whitelist.address);
+
+  const drip = hre.ethers.utils.formatEther(await whitelist.drip());
+  console.log("drip", drip);
+
+  console.log('whitelist balance before adding:', parseInt(hre.ethers.utils.formatEther(prevBalance)));
+
+  const prevPlayers = await whitelist.numPlayers();
+  console.log('num whitelisted players before adding:', prevPlayers.toNumber());
+
+  const allowedAccounts = await whitelist.bulkGetWhitelistIds(hre.ethers.constants.Zero, prevPlayers);
+
+  let addresses = "";
+  console.log("path", args.path);
+  addresses = fs.readFileSync(args.path, 'utf8')
+  // remove new line 
+  addresses = addresses.replace(/\n|\r/g,'');
+  if(!addresses) {
+    console.log("failed to read addresses");
+    return;
+  }
+
+  const finalAddresses = addresses.split(',');
+
+  // also filter for whitelisted addresses that are already in game.
+  let validAddresses = finalAddresses
+      .filter(hre.ethers.utils.isAddress)
+      .filter(acc => !allowedAccounts.includes(acc)); // Filter out accounts that are already whitelisted.
+  
+  const slice = 100;
+
+  validAddresses = validAddresses.slice(0,500);
+
+  console.log("total players to add", validAddresses.length);
+
+  if( (parseFloat(drip) * validAddresses.length) > parseFloat(hre.ethers.utils.formatEther(prevBalance))) {
+    console.log("not enough eth in contract. Add more before whitelisting");
+    return;
+  }
+  
+  if(validAddresses.length === 0) {
+    console.log("no valid addresses to register");
+    return;
+  }
+
+  // batch into chunks of 100
+  for(let i = 0; i < validAddresses.length; i+=slice) {
+    console.log(`examining index ${i} to ${i + slice}`);
+    const addressChunk = validAddresses.slice(i, i + slice);
+
+    console.log("chunk players to add", addressChunk.length);
+    if(addressChunk.length == 0) {
+      console.log("no addresses to chunk")
+      continue;
+    }
+
+    const prevBalance = await hre.ethers.provider.getBalance(whitelist.address);
+
+    console.log("drip", drip);
+    console.log(parseFloat(drip));
+    console.log("expected amount in whitelist", parseFloat(drip) * addressChunk.length);
+  
+    if( parseFloat(drip) * addressChunk.length > parseFloat(hre.ethers.utils.formatEther(prevBalance))) {
+      console.log("not enough eth in contract. Add more before whitelisting");
+      break;
+    }
+    console.log('whitelist balance before adding:', parseInt(hre.ethers.utils.formatEther(prevBalance)));
+
+    const prevPlayers = await whitelist.numPlayers();
+    console.log('num whitelisted players before adding:', prevPlayers.toNumber());
+
+    const addTx = await whitelist.addAndDripPlayers(addressChunk);
+    await addTx.wait();
+
+    const currBalance = await hre.ethers.provider.getBalance(whitelist.address);
+    console.log('whitelist balance after adding:', hre.ethers.utils.formatEther(currBalance));
+
+    const currPlayers = await whitelist.numPlayers();
+    console.log('num whitelisted players after adding:', currPlayers.toNumber());
+
+    const newPlayers = currPlayers.sub(prevPlayers).toNumber()
+    if(newPlayers != addressChunk.length) {
+      console.log("WARNING: not all players in list were successfully added");
+    }
+    
+    const sentAmount = hre.ethers.utils.formatEther(prevBalance.sub(currBalance));
+
+    console.log(`[${new Date()}] Registered ${newPlayers} players with $${sentAmount} xDAI.`);
+  }
+}
+
+task(
   'whitelist:register',
   'add address to whitelist contract with given key if provided, or on-the-fly-generated key if not provided'
 )
